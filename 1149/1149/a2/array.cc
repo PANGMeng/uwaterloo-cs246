d4276eb6f252dbf387ad838384c5e26a768146e9
Jeremy Rutledge <jerrutledge@gmail.com>
2014-10-21 14:41:03 -0400

    updating to a3q1 implementation

--- 1149/1149/a2/array.cc
+++
@@ -1,6 +0,0 @@
-struct IntArray {
-  int size;  // number of elements the array currently holds
-  int capacity;  // number of elements the array could hold, given current
-                 // memory allocation to contents
-  int *contents;
-};
